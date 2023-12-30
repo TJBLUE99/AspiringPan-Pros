8bdf4f2c2d3ddf7bb4f5ae3c5144d48f15e4a181
Harsh Vardhan <harshfebruary@gmail.com>
2023-12-30 15:57:45 +0530

    pr changes

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <GlobalRouter />
-        </Routes>
+        <GlobalRouter />
       </BrowserRouter>
 
       {/* <Navbar />
